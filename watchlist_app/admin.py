62ce5820412037ed9d60bfdd3c1d4d75b914b873
Uday Pratap <udaypratap169@gmail.com>
2023-03-31 22:04:47 +0530

    drf project started

---
+++ watchlist_app/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from watchlist_app.models import Movie
+
+
+admin.site.register(Movie)
---
+++ watchlist_app/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+from watchlist_app.models import Movie
+
+
+def movie_list(request):
+    
+    movies = Movie.objects.all()
+    data = {
+        'movies': list(movies.values())
+    }
+    return JsonResponse(data)
+
+
+def movie_details(request, pk):
+    
+    movie = Movie.objects.get(pk=pk)
+    
+    data = {
+        'id': movie.id,
+        'name': movie.name,
+        'description': movie.description,
+        'active': movie.active
+    }
+    return JsonResponse(data)
+    
